3e7332c39b96c769997412527c8bd2f8b09c35bb
5910501950 <somapa.sakara@gmail.com>
2017-11-03 11:29:42 +0700

    add class Pacman and move in 2d

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,34 +1,41 @@
 package com.mygdx.game;
 
 import java.awt.RenderingHints.Key;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter{
 	private PacmanGame pacmanGame;
 	
 	private Texture pacmanImg;
-	private int x;
-	private int y;
+	
+	private Pacman pacman;
 	
 	public GameScreen(PacmanGame pacmanGame) {
 		this.pacmanGame = pacmanGame;
+		
 		pacmanImg = new Texture("pacman.png");
-		x = 100;
-		y = 100;
+		
+		pacman = new Pacman(100, 100);
 	}
 	
 	public void update(float delta) {
+		if(Gdx.input.isKeyPressed(Keys.UP)) {
+			pacman.move(Pacman.DIRECTION_UP);
+		}
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+			pacman.move(Pacman.DIRECTION_DOWN);
+		}
 		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			x -= 10;
+			pacman.move(Pacman.DIRECTION_LEFT);
 		}
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			x += 10;
+			pacman.move(Pacman.DIRECTION_RIGHT);
 		}
 	}
 	
@@ -39,7 +46,8 @@ public class GameScreen extends ScreenAdapter{
         update(delta);
 		SpriteBatch batch = pacmanGame.batch;
 		batch.begin();
-		batch.draw(pacmanImg, x, y);
+		Vector2 pos = pacman.getPosition();
+		batch.draw(pacmanImg, pos.x, pos.y);
 		batch.end();
     }
 }
