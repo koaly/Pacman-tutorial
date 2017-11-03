b1c864e1eaef3e408f057e4231f364278b5bf4e6
5910501950 <somapa.sakara@gmail.com>
2017-11-03 11:04:21 +0700

    move pacman

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,6 +1,11 @@
 package com.mygdx.game;
 
+import java.awt.RenderingHints.Key;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -8,18 +13,33 @@ public class GameScreen extends ScreenAdapter{
 	private PacmanGame pacmanGame;
 	
 	private Texture pacmanImg;
+	private int x;
+	private int y;
 	
 	public GameScreen(PacmanGame pacmanGame) {
 		this.pacmanGame = pacmanGame;
 		pacmanImg = new Texture("pacman.png");
+		x = 100;
+		y = 100;
+	}
+	
+	public void update(float delta) {
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x -= 10;
+		}
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x += 10;
+		}
 	}
 	
 	@Override
     public void render(float delta) {
-        //System.out.println("Hello " + delta);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        update(delta);
 		SpriteBatch batch = pacmanGame.batch;
 		batch.begin();
-		batch.draw(pacmanImg, 100, 100);
+		batch.draw(pacmanImg, x, y);
 		batch.end();
     }
 }
