d1af2582e228c5d743176efe6b44a7095a5e73a0
Doki <talesdomo@gmail.com>
2017-11-05 22:23:36 +0700

    class Maze and MazeRenderer + BLOCKSIZE const

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -3,14 +3,20 @@ package com.mygdx.game;
 public class World {
 	private Pacman pacman;
 	private PacmanGame pacmanGame;
+	private Maze maze;
 	
 	World(PacmanGame pacmanGame){
 		this.pacmanGame = pacmanGame;
 		
-		pacman = new Pacman(100, 100);
+		pacman = new Pacman(60, 60);
+		maze = new Maze();
 	}
 	
 	Pacman getPacman() {
 		return pacman;
+	}
+	
+	Maze getMaze() {
+		return maze;
 	}
 }
--- core/src/com/mygdx/game/WorldRenderer.java
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class WorldRenderer {
+	
+	public static final int BLOCK_SIZE = 40;
+	
 	private PacmanGame pacmanGame;
 	
 	private Texture pacmanImg;
@@ -13,6 +16,8 @@ public class WorldRenderer {
 	
 	SpriteBatch batch;
 	
+	private MazeRenderer mazeRenderer;
+	
 	public WorldRenderer(PacmanGame pacmanGame, World world) {
 		this.pacmanGame = pacmanGame;
 		batch = pacmanGame.batch;
@@ -20,12 +25,16 @@ public class WorldRenderer {
 		this.world = world;
 		
 		pacmanImg = new Texture("pacman.png");
+		
+		mazeRenderer = new MazeRenderer(pacmanGame.batch, world.getMaze());
 	}
 	
 	public void render(float delta) {
+		mazeRenderer.render();
+		
 		batch.begin();
 		Vector2 pos = world.getPacman().getPosition();
-		batch.draw(pacmanImg, pos.x, pos.y);
+		batch.draw(pacmanImg, pos.x - BLOCK_SIZE/2, PacmanGame.HEIGHT - pos.y - BLOCK_SIZE/2);
 		batch.end();
 	}
 }
